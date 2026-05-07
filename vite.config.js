fab2c5aa2ec63db3ad03956542d7526ff264932b
academialuchorolandopadel <abcconstruccionesenseco@gmail.com>
2026-05-07 09:17:31 -0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Cambiá 'academia-lr' por el nombre exacto de tu repositorio en GitHub
-export default defineConfig({
-  plugins: [react()],
-  base: '/academia-lr/',
-})
