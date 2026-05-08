3f423d0e0ceea7ee47abcbe6c8544c44c0f0ef9b
academialuchorolandopadel <abcconstruccionesenseco@gmail.com>
2026-05-07 21:49:20 -0300

    Remove comment regarding GitHub repository name
    Remove comment about changing repository name.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// Cambiá 'academia-lr' por el nombre exacto de tu repositorio en GitHub
 export default defineConfig({
   plugins: [react()],
   base: '/academia-lr/',
